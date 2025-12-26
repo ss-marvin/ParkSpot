637a0af21fc5d1724fddf39e645d2e41a76cd330
ss-marvin <marvinsawasa@gmail.com>
2025-12-26 19:32:12 +0100

    Initial Commit

---
+++ ParkSpot/ParkSpotApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParkSpotApp.swift
+//  ParkSpot
+//
+//  Created by Marvin Sawasa on 2025-12-26.
+//
+
+import SwiftUI
+
+@main
+struct ParkSpotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
